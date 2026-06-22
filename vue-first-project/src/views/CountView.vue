2a5bfe8c4a36d500225a6412f08b192e72190ee5
ronald <ronald@gmail.com>
2026-06-21 21:08:47 -0400

    add CountView

---
+++ vue-first-project/src/views/CountView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
+
